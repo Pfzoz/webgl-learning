c522672484dcefe738f6129daee18e0638a46af0
pfzoz <pedrozoz.sizaan@gmail.com>
2025-01-01 21:11:10 -0300

    feat: cards enum and gitignore to hide heavy assets

--- src/objects/meshes/card.ts
+++ src/objects/meshes/card.ts
@@ -4,6 +4,11 @@ const cardGeometry = new THREE.BoxGeometry(1, 0.1, 1);
 const cardMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 const cardMesh = new THREE.Mesh(cardGeometry, cardMaterial);
 
-const card = () => {};
+export enum CardType {
+    SPADES_10,
+    DIAMONDS_10,
+}
+
+const card = (texture: CardType) => {};
 
 export { card };
